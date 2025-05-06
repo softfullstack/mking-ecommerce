3df7d692a1727008f3a985733c137a298289951c
mario tip <ing_mariomcs@outlook.com>
2025-05-05 23:17:41 -0600

    feat: refactor CartItem interface and enhance CartStore structure

--- src/interfaces/CarInterface.tsx
+++ src/interfaces/CarInterface.tsx
@@ -1,9 +1,16 @@
-export interface Product {
-    id: string
-    name: string
-    price: number
+import { Product } from "./ProductInterface"
+
+interface CartItem extends Product {
     quantity: number
-    color: string
     size: string
-    images: string[]
+    color: string
+}
+export interface CartStore {
+    items: CartItem[]
+    totalItems: number
+    totalPrice: number
+    addToCart: (product: Product, quantity: number, size: string, color: string) => void
+    removeFromCart: (itemIndex: number) => void
+    updateQuantity: (itemIndex: number, newQuantity: number) => void
+    clearCart: () => void
 }
